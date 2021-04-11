ac75191ed8d05e56e4926f5fe987a9730d2e7905
Rachid ETTABAAI <ettabaai.rachid@gmail.com>
2021-04-11 22:17:15 +0200

    add controller and template for index page

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,10 +2,17 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class IndexController extends AbstractController
 {
+    /**
+     * @Route("/",name="index")
+     *
+     * @return Response
+     */
     public function index()
     {
         return $this->render("index/index.html.twig");
