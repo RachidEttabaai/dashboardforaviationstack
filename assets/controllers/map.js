464f3b07cc4b3d16cf5cb32f448112f09a260ae9
Rachid ETTABAAI <ettabaai.rachid@gmail.com>
2021-04-13 01:15:49 +0200

    add leaflet

--- assets/controllers/map.js
+++ assets/controllers/map.js
@@ -5,8 +5,10 @@ let map = L.map("map", {
     maxZoom: 19,
 });
 
-let cartodbAttribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>';
+let cartodbAttribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attribution">CARTO</a>';
 
-let positron = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', ).addTo(map);
+let positron = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+    attribution: cartodbAttribution
+}).addTo(map);
 
-map.setView([0, 0]);
+map.setView([0, 0],3);
