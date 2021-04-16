a42e0cbcca0c51786e06c2c8afa7e5097526e7c0
Rachid ETTABAAI <ettabaai.rachid@gmail.com>
2021-04-16 02:17:33 +0200

    add service for apirequest and remove package from package.json

---
+++ src/Service/ApiCallService.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Contracts\HttpClient\ResponseInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class ApiCallService
+{
+    private HttpClientInterface $httpclient;
+
+    private string $apiurl;
+
+    public function __construct(string $apiurl)
+    {
+        $this->apiurl = $apiurl;
+    }
+
+    /**
+     * Get Datas from API with the httpclient component
+     *
+     * @return ResponseInterface
+     */
+    private function getDatasFromAPI(): ResponseInterface
+    {
+        $request = $this->httpclient->request(
+            "GET",
+            $this->apiurl);
+
+        return $request;
+    }
+    
+    /**
+     * Show datas fril an API in an array
+     *
+     * @return array
+     */
+    public function showDatasFromAPI(): array
+    {
+        return $this->getDatasFromAPI()->toArray();
+    } 
+}
