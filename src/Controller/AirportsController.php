7e4effb378892ec99c4e38aec497d83954c16e39
Rachid ETTABAAI <ettabaai.rachid@gmail.com>
2021-04-22 07:30:06 +0200

    update controller for getting datas

--- src/Controller/AirportsController.php
+++ src/Controller/AirportsController.php
@@ -6,6 +6,8 @@ use App\Service\ApiCallService;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
 /**
  * @Route("/api/airports")
  */
@@ -22,10 +24,12 @@ class AirportsController extends AbstractController
     {
         $apiCallService->setApiUrl("http://api.aviationstack.com/v1/airports?access_key=b930884297de709e3f1531ffda8e936a&limit=6471");
         
-        $response = new JsonResponse(json_encode($apiCallService->showDatasFromAPI()),
-                                     200,
-                                     ["Content-Type" => "application/json"]);
-        $response->setEncodingOptions($response->getEncodingOptions()|JSON_PRETTY_PRINT);
+        $response = new Response();
+
+        $response->setContent(json_encode($apiCallService->showDatasFromAPI()));
+
+        $response->headers->set("Content-Type", "application/json");
+        $response->headers->set("Access-Control-Allow-Origin", "*");
 
         return $response;
         
