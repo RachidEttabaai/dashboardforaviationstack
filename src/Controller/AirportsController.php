7a80279227ad8b35f97ec7207f2434ed26b32457
Rachid ETTABAAI <ettabaai.rachid@gmail.com>
2021-04-22 07:16:48 +0200

    update controller for getting datas

--- src/Controller/AirportsController.php
+++ src/Controller/AirportsController.php
@@ -5,15 +5,13 @@ namespace App\Controller;
 use App\Service\ApiCallService;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AirportsController extends AbstractController
 {
     /**
      * Get all airports datas from aviationstack API
-     * @Route("/allairports",name="allairports")
-     * @Method("GET")
+     * @Route("/allairports",name="allairports", methods={"GET"})
      * 
      * @param ApiCallService $apiCallService
      * @return JsonResponse
@@ -22,8 +20,9 @@ class AirportsController extends AbstractController
     {
         $apiCallService->setApiUrl("http://api.aviationstack.com/v1/airports?access_key=b930884297de709e3f1531ffda8e936a&limit=6471");
         
-        $response = new JsonResponse($apiCallService->showDatasFromAPI());
-        dd($apiCallService->showDatasFromAPI());
+        $response = new JsonResponse(json_encode($apiCallService->showDatasFromAPI()),
+                                     200,
+                                     ["Content-Type" => "application/json"]);
         $response->setEncodingOptions($response->getEncodingOptions()|JSON_PRETTY_PRINT);
 
         return $response;
