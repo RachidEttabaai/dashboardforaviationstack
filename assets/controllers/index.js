78c55fda3917c935993c5ab4f80655db6c9d99f8
ETTABAAI Rachid <ettabaai.rachid@gmail.com>
2021-04-30 09:59:42 +0200

    add envent on click button

--- assets/controllers/index.js
+++ assets/controllers/index.js
@@ -1,5 +1,6 @@
 import {initMap} from "./map.js";
 import {addLegend} from "./addLegendtoMap.js";
+import {showAllAirports} from "./showAllAirports.js";
 
 let map = initMap();
 addLegend(map);
---
+++ assets/controllers/showAllAirports.js
@@ -0,0 +1,8 @@
+export function showAllAirports(){
+
+    document.querySelector("#show-allairports").addEventListener("click",function(){
+        console.log("btn show-allairports clicked");
+    });
+    
+}
+
