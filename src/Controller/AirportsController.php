c018f8a15368965a39b55a21d506f3e8c96f3878
ETTABAAI Rachid <ettabaai.rachid@gmail.com>
2021-05-04 05:49:38 +0200

    update airports controller

--- src/Controller/AirportsController.php
+++ src/Controller/AirportsController.php
@@ -13,20 +13,42 @@ use Symfony\Component\HttpFoundation\Response;
  */
 class AirportsController extends AbstractController
 {
+    private string $apiurl = "http://api.aviationstack.com/v1/airports?access_key=b930884297de709e3f1531ffda8e936a&limit=6471";
+    
     /**
-     * Get all airports datas from aviationstack API
+     * Get the aviation stack API url
+     *
+     * @return string
+     */
+    public function getApiUrl()
+    {
+        return $this->apiurl;
+    }
+    
+    /**
+     * Get all airports datas using aviationstack API
+     * @param  ApiCallService $apiCallService
+     * @return string
+     */
+    private function getAirportsDatasFromApi(ApiCallService $apiCallService)
+    {
+        $apiCallService->setApiUrl($this->getApiUrl());
+        
+        return json_encode($apiCallService->showDatasFromAPI());
+
+    }
+
+    /**
+     * Return all airports datas using aviationstack API 
      * @Route("/all",name="allairports", methods={"GET"})
-     * 
      * @param ApiCallService $apiCallService
      * @return JsonResponse
      */
     public function getAll(ApiCallService $apiCallService)
     {
-        $apiCallService->setApiUrl("http://api.aviationstack.com/v1/airports?access_key=b930884297de709e3f1531ffda8e936a&limit=6471");
-        
         $response = new Response();
 
-        $response->setContent(json_encode($apiCallService->showDatasFromAPI()));
+        $response->setContent($this->getAirportsDatasFromApi($apiCallService));
 
         $response->headers->set("Content-Type", "application/json");
         $response->headers->set("Access-Control-Allow-Origin", "*");
