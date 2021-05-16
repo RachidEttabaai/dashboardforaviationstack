c2998eeb25ef224e89245039d0486da284215908
ETTABAAI <ettabaai.rachid@gmail.com>
2021-05-17 00:21:33 +0200

    add button event for hiding markers on map

---
+++ assets/controllers/hideAllAirports.js
@@ -0,0 +1,11 @@
+let $ = require("jquery");
+import "leaflet";
+
+export function hideAllAirports(map){
+
+    document.querySelector("#hide-allairports").addEventListener("click",function(){
+       
+        console.log(map.__layers);
+        
+    });
+}
--- assets/controllers/index.js
+++ assets/controllers/index.js
@@ -1,8 +1,10 @@
 import {initMap} from "./map.js";
 import {addLegend} from "./addLegendtoMap.js";
 import {showAllAirports} from "./showAllAirports.js";
+import { hideAllAirports } from "./hideAllAirports.js";
 
 let map = initMap();
 addLegend(map);
 document.getElementById("hide-allairports").disabled = true;
 showAllAirports(map);
+hideAllAirports(map);
