c84813d9f83ff512e98f5c9947d53cefc376f5d5
ETTABAAI <ettabaai.rachid@gmail.com>
2021-05-17 00:24:27 +0200

    add button event for hiding markers on map

--- assets/controllers/hideAllAirports.js
+++ assets/controllers/hideAllAirports.js
@@ -5,7 +5,7 @@ export function hideAllAirports(map){
 
     document.querySelector("#hide-allairports").addEventListener("click",function(){
        
-        console.log(map.__layers);
+        console.log(map);
         
     });
 }
