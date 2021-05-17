db332d729cbdd293e57dcbaff39af1e3c9de4ed6
ETTABAAI <ettabaai.rachid@gmail.com>
2021-05-17 02:22:06 +0200

    add button for hiding markers on map

--- assets/controllers/hideAllAirports.js
+++ assets/controllers/hideAllAirports.js
@@ -3,7 +3,7 @@ import "leaflet";
 
 export function hideAllAirports(map,markers){
 
-    console.log(markers)
+    //console.log(markers)
     for(let i=0;i<markers.length;i++) {
         map.removeLayer(markers[i]);
     }  
